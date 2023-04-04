eb26a0b20dd657ca9e6235da7ec3cd2202d2d3be
yang <1421953220@qq.com>
2023-04-05 00:25:59 +0800

    log4j2+slf4j

--- src/main/java/com/example/springboot/Controller/HelloController.java
+++ src/main/java/com/example/springboot/Controller/HelloController.java
@@ -3,12 +3,21 @@ package com.example.springboot.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String hello() {
+        LOGGER.trace("hello, I am trace!");
+        LOGGER.debug("hello, I am debug!");
+        LOGGER.info("hello, I am info!");
+        LOGGER.warn("hello, I am {}!", "warn");
+        LOGGER.error("hello, I am {}!", "error");
         return "Hello Spring Boot hhhhh!";
     }
 
